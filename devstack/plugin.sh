eb0ccb65f6c6bc71767c19b57f53e9fc2982b93a
Hayde Martinez <hayde.martinez.landa@intel.com>
2018-10-22 11:50:57 -0500

    Style added

--- devstack/plugin.sh
+++ devstack/plugin.sh
@@ -1,7 +1,15 @@
-LIBDIR=$DEST/stx-gui/devstack/lib
+#!/bin/bash# devstack/plugin.sh
+# Triggers stx-gui specific functions to install and configure starlingx dashboard
+# Dependencies:
+#
+# - ``functions`` file
+# - ``DATA_DIR`` must be defined# ``stack.sh`` calls the entry points in this order:
+#
 
+LIBDIR=$DEST/stx-gui/devstack/lib
 source $LIBDIR/starlingx-dashboard.sh
 # check for service enabled
+
 if is_service_enabled stx-gui; then
 
     if [[ "$1" == "stack" && "$2" == "install" ]]; then
@@ -11,10 +19,20 @@ if is_service_enabled stx-gui; then
 
     elif [[ "$1" == "stack" && "$2" == "post-config" ]]; then
 	echo_summary "Configuring stx-gui"
-    fi
 
+    elif [[ "$1" == "stack" && "$2" == "extra" ]]; then
+        # Initialize and start the service
+        echo_summary "Initializing and start stx-gui"
+
+    elif [[ "$1" == "stack" && "$2" == "test-config" ]]; then
+        # do sanity test
+        echo_summary "do test-config"
+    fi
     if [[ "$1" == "unstack" ]]; then
-	echo_summary "Unstack stx-gui"
+        # Shut down services
+        echo_summary "Stop stx-gui service"
+    fi
+    if [[ "$1" == "clean" ]]; then
 	clean_stx-dashboard
     fi
 fi
