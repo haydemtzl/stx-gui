6f27e3dd4d5296c66f036ba03bef34b64d1d3f03
Hayde Martinez <hayde.martinez.landa@intel.com>
2019-01-30 15:42:42 -0600

    devstack update

--- devstack/plugin.sh
+++ devstack/plugin.sh
@@ -1,24 +1,28 @@
 #!/bin/bash# devstack/plugin.sh
 # Triggers stx-gui specific functions to install and configure starlingx dashboard
-# Dependencies:
 #
-# - ``functions`` file
-# - ``DATA_DIR`` must be defined# ``stack.sh`` calls the entry points in this order:
 #
 
-LIBDIR=$DEST/stx-gui/devstack/lib
-source $LIBDIR/starlingx-dashboard.sh
-# check for service enabled
+function install_stx_dashboard {
+    setup_develop ${STX_GUI_DIR}
+}
 
-if is_service_enabled stx-gui; then
+function configure_stx_dashboard {
+    cp -a ${STX_GUI_DIR}/starlingx-dashboard/starlingx-dashboard/starlingx-dashboard/enabled/* $DEST/horizon/openstack_dashboard/local/enabled
+    cp -a ${STX_GUI_DIR}/starlingx-dashboard/starlingx-dashboard/starlingx-dashboard/local/local_settings.d/_30_stx_local_settings.py $DEST/horizon/openstack_dashboard/local/local_settings.d/
+}
+
+#check for service enabled
+if is_service_enabled horizon && is_service_enabled stx-gui; then
 
     if [[ "$1" == "stack" && "$2" == "install" ]]; then
-	# Perform installation of service source
-	echo_summary "Installing stx-gui"
-	install_stx-dashboard
+        # Perform installation of service source
+        echo_summary "Installing stx-gui"
+        install_stx_dashboard
 
     elif [[ "$1" == "stack" && "$2" == "post-config" ]]; then
 	echo_summary "Configuring stx-gui"
+        configure_stx_dashboard
 
     elif [[ "$1" == "stack" && "$2" == "extra" ]]; then
         # Initialize and start the service
@@ -33,6 +37,8 @@ if is_service_enabled stx-gui; then
         echo_summary "Stop stx-gui service"
     fi
     if [[ "$1" == "clean" ]]; then
-	clean_stx-dashboard
+	rm -f $DEST/horizon/openstack_dashboard/local/enabled/*starlingx*
+	rm -f $DEST/horizon/openstack_dashboard/local/enabled/_2212_WRS_dc_admin_software_management_panel.py
+	rm -f $DEST/horizon/openstack_dashboard/local/local_settings.d/_30_stx_local_settings.py
     fi
 fi
